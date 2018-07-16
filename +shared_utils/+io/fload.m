a10c74dacf3334b64446471582e2204c767938eb
nfagan <nick@fagan.org>
2018-07-16 11:58:36 -0400

    update fload to allow variable spec

--- +shared_utils/+io/fload.m
+++ +shared_utils/+io/fload.m
@@ -1,4 +1,4 @@
-function out = fload(fname)
+function out = fload(fname, varname)
 
 %   FLOAD -- Load in and assign to variable.
 %
@@ -6,8 +6,13 @@ function out = fload(fname)
 %     assigns to the variable `out`. 'file1.mat' cannot contain multiple 
 %     variables.
 %
+%     out = ... fload( ..., VARNAME ); loads the variable `VARNAME` and
+%     assigns to the variable `out`. `VARNAME` must be a variable contained
+%     in the file.
+%
 %     IN:
 %       - `fname` (char) -- .mat file to load.
+%       - `varname` (char) |OPTIONAL|
 %     OUT:
 %       - `out` (/any/) -- Loaded data.
 
@@ -19,8 +24,13 @@ assert__file_exists( fname, 'the .mat file' );
 var = load( fname );
 fs = fieldnames( var );
 
-assert( numel(fs) == 1, 'More than one variable saved in ''%s''.', fname );
-
-out = var.(fs{1});
+if ( nargin == 1 )
+  assert( numel(fs) == 1, 'More than one variable saved in ''%s''.', fname );
+  out = var.(fs{1});
+else
+  assert__isa( varname, 'char', 'the variable name' );
+  assert( isfield(var, varname), 'Variable "%s" does not exist.', varname );
+  out = var.(varname);
+end
 
 end
