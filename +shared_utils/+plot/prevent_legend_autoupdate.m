8de32abe8b4f4ec02578fa3efd24e638817a087c
nfagan <nick@fagan.org>
2018-07-24 14:24:49 -0400

    add leg autoupdate

---
+++ +shared_utils/+plot/prevent_legend_autoupdate.m
@@ -0,0 +1,18 @@
+function f = prevent_legend_autoupdate(f)
+
+%   PREVENT_LEGEND_AUTOUPDATE -- Prevent legend from auto-updating.
+%
+%     IN:
+%       - `f` (figure)
+%     OUT:
+%       - `f` (figure)
+
+if ( nargin < 1 )
+  f = gcf();
+end
+
+for i = 1:numel(f)
+  set( f(i), 'defaultLegendAutoUpdate', 'off' );
+end
+
+end
