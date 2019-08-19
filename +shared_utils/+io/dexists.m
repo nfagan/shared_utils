73eb5621daf5b92483b088309a0896d80a68b74d
nfagan <nick@fagan.org>
2019-08-19 14:15:05 -0400

    update help text

--- +shared_utils/+io/dexists.m
+++ +shared_utils/+io/dexists.m
@@ -2,11 +2,19 @@ function tf = dexists( file_path )
 
 %   DEXISTS -- True if a directory exists.
 %
-%     IN:
-%       - `dir_path` (char)
-%     OUT:
-%       - `tf` (logical)
+%     tf = shared_utils.io.dexists( path ); returns true if the folder given
+%     by the char vector `path` exists.
+%
+%     tf = shared_utils.io.dexists( paths ); for the cell array of strings
+%     `paths` returns true for elements of `paths` that are existing
+%     directories.
+%
+%     See also shared_utils.io.fexists, exist
 
-tf = exist( file_path, 'dir' ) == 7;
+if ( ischar(file_path) )
+  tf = exist( file_path, 'dir' ) == 7;
+else
+  tf = cellfun( @(x) exist(x, 'dir') == 7, file_path );
+end
 
 end
--- +shared_utils/+io/fexists.m
+++ +shared_utils/+io/fexists.m
@@ -2,11 +2,19 @@ function tf = fexists( file_path )
 
 %   FEXISTS -- True if a file exists.
 %
-%     IN:
-%       - `file_path` (char)
-%     OUT:
-%       - `tf` (logical)
+%     tf = shared_utils.io.fexists( path ); returns true if the file given
+%     by the char vector `path` exists.
+%
+%     tf = shared_utils.io.fexists( paths ); for the cell array of strings
+%     `paths` returns true for elements of `paths` that are existing
+%     files.
+%
+%     See also shared_utils.io.dexists, exist
 
-tf = exist( file_path, 'file' ) == 2;
+if ( ischar(file_path) )
+  tf = exist( file_path, 'file' ) == 2;
+else
+  tf = cellfun( @(x) exist(file_path, 'file') == 2, file_path );
+end
 
 end
